fdfd1d2bfd93ad00b7a7b355917e8dea89983212
mukesh <mukesh@learnyst.com>
2023-11-13 11:22:26 +0530

    init 2

---
+++ controllers/tourController.js
@@ -0,0 +1,93 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+
+const checkID = (req, res, next, val) => {
+  console.log(`Tour id is ${val}`)
+  
+  if (tours.length - 1 < +req.params.id) {
+    return res.status(401).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+  next()
+}
+
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    requestedTime: req.requestTime,
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+const getTour = (req, res) => {
+  const tour = tours.find((el) => el.id === +req.params.id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+const checkBody = (req, res, next) => {
+  if(!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price'
+    })
+  }
+  next()
+}
+
+const createTour = (req, res) => {
+  const tourId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: tourId }, req.body);
+
+  const newTours = [...tours, newTour];
+  // tours.push(newTour) // both are correct!
+
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/tours-simple.json`,
+    JSON.stringify(newTours),
+    (err) => {
+        if(err) {
+            console.log('Failed to write file');
+        }
+    }
+  );
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: newTour,
+    },
+  });
+};
+
+const updateTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: `<Updated tour here...>`,
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+module.exports = { getAllTours, getTour, createTour, updateTour, deleteTour, checkID, checkBody };
---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+const getAllUsers = (req, res) => {
+    console.log(req);
+    res.status(500).json({
+      status: 'fail',
+      message: 'User has not implemented yet!',
+    });
+  };
+  const createUser = (req, res) => {
+    res.status(500).json({
+      status: 'fail',
+      message: 'User has not implemented yet!',
+    });
+  };
+  const getUser = (req, res) => {
+    res.status(500).json({
+      status: 'fail',
+      message: 'User has not implemented yet!',
+    });
+  };
+  const updateUser = (req, res) => {
+    res.status(500).json({
+      status: 'fail',
+      message: 'User has not implemented yet!',
+    });
+  };
+  const deleteUser = (req, res) => {
+    res.status(500).json({
+      status: 'fail',
+      message: 'User has not implemented yet!',
+    });
+  };
+  
+
+  module.exports = { getAllUsers, createUser, getUser, updateUser, deleteUser }
