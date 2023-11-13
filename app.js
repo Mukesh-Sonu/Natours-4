48a4d30260022048a06922ad3262b8b15788258e
mukesh <mukesh@learnyst.com>
2023-11-13 12:51:09 +0530

    lint install

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const userRouter = require('./routes/userRoutes')
 const app = express();
 
 app.use(express.json());
-app.use(morgan('dev'));
+
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
